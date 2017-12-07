ffcc88dcaa02e0063cf59ba70bd842ddf45b1dcb
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2017-12-07 12:03:00 +0100

    Day7-step1
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

---
+++ day7/advent7_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var sampleInput = `pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)
+`
+
+func TestExitRightAway(t *testing.T) {
+	assert.Equal(t, "", advent71(""))
+}
+
+func TestWithSampleInput(t *testing.T) {
+	assert.Equal(t, "tknk", advent71(sampleInput))
+}
+
+func TestFirstExercice(t *testing.T) {
+	text, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(advent71(string(text)))
+}
+
+func advent71(input string) string {
+	if input == "" {
+		return ""
+	}
+	lefts := []string{}
+	rights := []string{}
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		indexFirstSpace := strings.Index(line, " ")
+		if indexFirstSpace != -1 {
+			word := line[0:indexFirstSpace]
+			lefts = append(lefts, word)
+		}
+		indexArrow := strings.Index(line, ">")
+		if indexArrow != -1 {
+			rightList := line[indexArrow+2 : len(line)]
+			listOfRight := strings.Split(rightList, ", ")
+			for _, right := range listOfRight {
+				rights = append(rights, right)
+			}
+		}
+	}
+	for _, left := range lefts {
+		found := false
+		for _, right := range rights {
+			if right == left {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return left
+		}
+	}
+	return ""
+}
