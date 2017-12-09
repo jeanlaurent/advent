55b5372e2a73c0afea9cc75f32e357ee635c7e18
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2017-12-09 10:27:49 +0100

    day9 part1
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- day9/src/main/java/advent17/Advent.java
+++ day9/src/main/java/advent17/Advent.java
@@ -3,7 +3,39 @@ package advent17;
 
 public class Advent
 {
-    int foobar(String input) {
-        return 0;
+    int nestedGroup;
+    int groups;
+    int score;
+
+    public Advent() {
+        this.groups = 0;
+        this.score = 0;
+        this.nestedGroup = 0;
+    }
+
+    public void parse(String line) {
+        boolean inGarbage = false;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '!') {
+                i++;
+                continue;
+            }
+            if (line.charAt(i) == '<') {
+                inGarbage = true;
+            }
+            if (line.charAt(i) == '>') {
+                inGarbage = false;
+            }
+            if (!inGarbage) {
+                if (line.charAt(i) == '{') {
+                    this.nestedGroup++;
+                    score += nestedGroup;
+                }
+                if (line.charAt(i) == '}') {
+                    groups++;
+                    nestedGroup--;
+                }
+            }
+        }
     }
 }
--- day9/src/test/java/advent17/AdventTest.java
+++ day9/src/test/java/advent17/AdventTest.java
@@ -2,12 +2,97 @@ package advent17;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AdventTest {
 
     @Test
-    public void is_zero() {
-        assertThat(new Advent().foobar("foo")).isEqualTo(0);
+    public void find_one_group() {
+        Advent advent = new Advent();
+        advent.parse("{}");
+        assertThat(advent.groups).isEqualTo(1);
+        assertThat(advent.score).isEqualTo(1);
     }
+
+    @Test
+    public void find_two_nested_group() {
+        Advent advent = new Advent();
+        advent.parse("{{{}}}");
+        assertThat(advent.groups).isEqualTo(3);
+        assertThat(advent.score).isEqualTo(6);
+    }
+
+    @Test
+    public void find_two_group_in_an_array() {
+        Advent advent = new Advent();
+        advent.parse("{{},{}}");
+        assertThat(advent.groups).isEqualTo(3);
+        assertThat(advent.score).isEqualTo(5);
+    }
+
+    @Test
+    public void find_six_groups() {
+        Advent advent = new Advent();
+        advent.parse("{{{},{},{{}}}}");
+        assertThat(advent.groups).isEqualTo(6);
+        assertThat(advent.score).isEqualTo(16);
+    }
+
+    @Test
+    public void find_one_group_with_big_garbage() {
+        Advent advent = new Advent();
+        advent.parse("{<{},{},{{}}>}");
+        assertThat(advent.groups).isEqualTo(1);
+    }
+
+    @Test
+    public void find_one_group_with_lots_of_tiny_garbage() {
+        Advent advent = new Advent();
+        advent.parse("{<a>,<a>,<a>,<a>}");
+        assertThat(advent.groups).isEqualTo(1);
+        assertThat(advent.score).isEqualTo(1);
+    }
+
+    @Test
+    public void find_five_group_with_lots_of_tiny_garbage() {
+        Advent advent = new Advent();
+        advent.parse("{{<a>},{<a>},{<a>},{<a>}}");
+        assertThat(advent.groups).isEqualTo(5);
+        assertThat(advent.score).isEqualTo(9);
+    }
+
+    @Test
+    public void find_2_groups_with_skips() {
+        Advent advent = new Advent();
+        advent.parse("{{<!>},{<!>},{<!>},{<a>}}");
+        assertThat(advent.groups).isEqualTo(2);
+    }
+
+    @Test
+    public void find_score_with_lots_of_escape_chars() {
+        Advent advent = new Advent();
+        advent.parse("{{<!!>},{<!!>},{<!!>},{<!!>}}");
+        assertThat(advent.score).isEqualTo(9);
+    }
+
+    @Test
+    public void find_score_with_weird_string() {
+        Advent advent = new Advent();
+        advent.parse("{{<a!>},{<a!>},{<a!>},{<ab>}}");
+        assertThat(advent.score).isEqualTo(3);
+    }
+
+    @Test
+    public void find_first_solution() throws IOException {
+        Advent advent = new Advent();
+        String input = new String(Files.readAllBytes(new File("./input.txt").toPath()));
+        advent.parse(input);
+        assertThat(advent.score).isEqualTo(14190);
+    }
+
 }
