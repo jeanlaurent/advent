edb2ac86b362c788f32e35865d40924167cd0389
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2017-12-09 10:49:57 +0100

    Less lines
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- day9/src/main/java/advent17/Advent.java
+++ day9/src/main/java/advent17/Advent.java
@@ -1,37 +1,36 @@
 package advent17;
 
-
 public class Advent
 {
     int nestedGroup = 0;
     int groups = 0;
-    int score = 0;
+    int score = 0;  
     int characters = 0;
 
     public void parse(String line) {
         boolean inGarbage = false;
         boolean openingGarbage;
-        for (int i = 0; i < line.length(); i++) {
+        for (int index = 0; index < line.length(); index++) {
             openingGarbage = false;
-            if (line.charAt(i) == '!') {
-                i++;
+            if (line.charAt(index) == '!') {
+                index++;
                 continue;
             }
-            if (line.charAt(i) == '<') {
+            if (line.charAt(index) == '<') {
                 openingGarbage = !inGarbage;
                 inGarbage = true;
             }
-            if (line.charAt(i) == '>') {
+            if (line.charAt(index) == '>') {
                 inGarbage = false;
             }
             if ((inGarbage) && (!openingGarbage)) {
                 characters++;
             } else {
-                if (line.charAt(i) == '{') {
+                if (line.charAt(index) == '{') {
                     this.nestedGroup++;
                     score += nestedGroup;
                 }
-                if (line.charAt(i) == '}') {
+                if (line.charAt(index) == '}') {
                     groups++;
                     nestedGroup--;
                 }
