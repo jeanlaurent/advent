80ccab1b18780bb95e4a26a1d3e587cb342c55d4
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2019-12-06 17:01:37 +0100

    day06-more-readable
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- 2019/day06/main.go
+++ 2019/day06/main.go
@@ -13,14 +13,8 @@ type planet struct {
 }
 
 func main() {
-	//rawListOfOrbits := load("orbit-test.txt")
-	rawListOfOrbits := load("orbit.txt")
-	orbits := [][]string{}
-	for _, orbitAsString := range strings.Split(rawListOfOrbits, "\n") {
-		orbit := strings.Split(string(orbitAsString), ")")
-		orbits = append(orbits, orbit)
+	orbits := loadOrbits()
 
-	}
 	orbitMap := make(map[string][]string)
 	for i := 0; i < len(orbits); i++ {
 		if orbitMap[orbits[i][0]] == nil {
@@ -29,37 +23,31 @@ func main() {
 			orbitMap[orbits[i][0]] = append(orbitMap[orbits[i][0]], orbits[i][1])
 		}
 	}
+
 	sum := 0
 	root := findOrbiters("COM", orbitMap, 1, &sum)
-	fmt.Println(root)
 	fmt.Println("step1 --->", sum)
+
 	// find orbiter of YOU, keep the full chain
 	pathToYou := make(map[string]int)
 	findPathTo(root, "YOU", pathToYou)
-	delete(pathToYou, "YOU")
-	fmt.Println(pathToYou)
+	youKeys := mapKeys(pathToYou)
+
 	// find orbiter of SAN, keep the full chain
 	pathToSan := make(map[string]int)
 	findPathTo(root, "SAN", pathToSan)
-	delete(pathToSan, "SAN")
-	fmt.Println(pathToSan)
+	sanKeys := mapKeys(pathToSan)
+
 	// Find a common node between the two
-	youKeys := make([]string, 0, len(pathToYou))
-	for key := range pathToYou {
-		youKeys = append(youKeys, key)
-	}
 	commonMax := 0
-	commonKey := ""
 	for _, key := range youKeys {
 		if pathToSan[key] != 0 {
 			if pathToSan[key] > commonMax {
 				commonMax = pathToSan[key]
-				commonKey = key
 			}
 		}
 	}
-	fmt.Println(commonKey, commonMax)
-	// add distance from SAN minus distance of common node + YOU and distance of common node
+
 	// find max value of YOU
 	youMax := 0
 	for _, key := range youKeys {
@@ -67,19 +55,24 @@ func main() {
 			youMax = pathToYou[key]
 		}
 	}
-	fmt.Println("Distance from YOU", youMax)
-	sanKeys := make([]string, 0, len(pathToSan))
-	for key := range pathToSan {
-		sanKeys = append(sanKeys, key)
-	}
+	// find max value of SAN
 	sanMax := 0
 	for _, key := range sanKeys {
 		if pathToSan[key] > sanMax {
 			sanMax = pathToSan[key]
 		}
 	}
-	fmt.Println("Distance from SAN", sanMax)
+
+	// add distance from SAN minus distance of common node + YOU and distance of common node
 	fmt.Println("step2 --->", sanMax-commonMax+youMax-commonMax)
+}
+
+func mapKeys(aMap map[string]int) []string {
+	sanKeys := make([]string, 0, len(aMap))
+	for key := range aMap {
+		sanKeys = append(sanKeys, key)
+	}
+	return sanKeys
 }
 
 func findPathTo(root planet, target string, path map[string]int) bool {
@@ -88,8 +81,9 @@ func findPathTo(root planet, target string, path map[string]int) bool {
 	}
 	for _, orbiter := range root.orbiters {
 		if findPathTo(orbiter, target, path) {
-			fmt.Println(orbiter.name)
-			path[orbiter.name] = orbiter.depth
+			if orbiter.name != target {
+				path[orbiter.name] = orbiter.depth
+			}
 			return true
 		}
 	}
@@ -103,6 +97,18 @@ func findOrbiters(rootName string, orbitMap map[string][]string, depth int, sum 
 		rootPlanet.orbiters = append(rootPlanet.orbiters, findOrbiters(orbiter, orbitMap, depth+1, sum))
 	}
 	return rootPlanet
+}
+
+func loadOrbits() [][]string {
+	//rawListOfOrbits := load("orbit-test.txt")
+	rawListOfOrbits := load("orbit.txt")
+	orbits := [][]string{}
+	for _, orbitAsString := range strings.Split(rawListOfOrbits, "\n") {
+		orbit := strings.Split(string(orbitAsString), ")")
+		orbits = append(orbits, orbit)
+
+	}
+	return orbits
 }
 
 func load(filename string) string {
